452fbc76bef20da3600435da83fedb26441b5055
Linh B Ngo <lngo@wcupa.edu>
2024-12-20 15:09:20 -0500

    Delete build.sh

--- build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -e
-
-docker image rm head:latest || true
-docker image rm compute:latest || true
-
-docker build -t head:latest ./docker/master
-docker build -t compute:latest ./docker/worker
