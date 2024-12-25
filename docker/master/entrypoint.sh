5b95bc0b3a3f8b3adc32dde40f8e960e7dc7321c
Linh B Ngo <lngo@wcupa.edu>
2024-12-25 15:52:44 -0500

    Update entrypoint.sh

--- docker/master/entrypoint.sh
+++ docker/master/entrypoint.sh
@@ -4,5 +4,5 @@ echo "---> Starting sshd on the slurmd..."
 /usr/sbin/sshd -e
 
 cd / && jupyter lab --port=8888 --NotebookApp.token='' --no-browser --ip=0.0.0.0 --allow-root &
-code-server code-server --bind-addr 0.0.0.0:8088
+code-server / --bind-addr 0.0.0.0:8088
 
