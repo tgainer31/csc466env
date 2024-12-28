5917f753370be8c92d38736cc9eadbf35a869f77
Linh Ngo <linh.b.ngo@gmail.com>
2024-12-28 05:20:40 -0500

    coder server by student

--- docker/master/entrypoint.sh
+++ docker/master/entrypoint.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 echo "---> Starting sshd on the slurmd..."
-/usr/sbin/sshd -e
+sudo /usr/sbin/sshd -e
 
-cd / && jupyter lab --port=8888 --NotebookApp.token='' --no-browser --ip=0.0.0.0 --allow-root &
-code-server / --bind-addr 0.0.0.0:8088
+cd /home/student/ 
+jupyter lab --port=8888 --NotebookApp.token='' --no-browser --ip=0.0.0.0 &
+code-server --bind-addr 0.0.0.0:8088
 
--- docker/master/install.sh
+++ docker/master/install.sh
@@ -2,10 +2,10 @@
 
 # setup code server
 curl -fsSL https://code-server.dev/install.sh | sh
-mkdir -p /root/.config/code-server/ 
-cp /build/code-server/config.yaml /root/.config/code-server/config.yaml
-mkdir -p /root/.local/share/code-server/User/ && \
-cp /build/code-server/settings.json /root/.local/share/code-server/User/ 
+#mkdir -p /root/.config/code-server/ 
+#cp /build/code-server/config.yaml /root/.config/code-server/config.yaml
+#mkdir -p /root/.local/share/code-server/User/ && \
+#cp /build/code-server/settings.json /root/.local/share/code-server/User/ 
 
 # setup Python packages
 source /opt/env/python3/bin/activate
