a185a286d675984c064f4fd9e56b80f2c6d6f2f7
Linh B Ngo <lngo@wcupa.edu>
2025-01-15 18:57:04 -0500

    Update install.sh

--- docker/master/install.sh
+++ docker/master/install.sh
@@ -6,7 +6,7 @@ sudo sh /home/student/build/code-server.sh --prefix=/usr/local/
 # setup Python packages
 source /opt/env/python3/bin/activate
 sudo pip install jupyter 
-sudo pip install jupyter-book mkdocs-material markdown-include mkdocs-table-reader-plugin mkdocs-glightbox   
+sudo pip install jupyter-book mkdocs-material markdown-include mkdocs-table-reader-plugin mkdocs-glightbox mkdocs-jupyter  
 
 sudo cp /home/student/build/entrypoint.sh /usr/local/bin/entrypoint.sh
 sudo chmod 755 /usr/local/bin/entrypoint.sh  
