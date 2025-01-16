a41e569bca014ad2e3c7ed44ee6c0fff53b7eb02
Linh B Ngo <lngo@wcupa.edu>
2025-01-16 15:47:23 -0500

    Delete docker/worker/install.sh

--- docker/worker/install.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# setup Python packages
-source /opt/env/python3/bin/activate
-pip install numpy matplotlib 
-    
-cp /build/entrypoint.sh /usr/local/bin/entrypoint.sh
-chmod 755 /usr/local/bin/entrypoint.sh  
-
