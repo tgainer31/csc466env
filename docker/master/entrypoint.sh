ade1e94b76349d2f3ac1380ff95cc5bbcd259d7a
Linh B Ngo <lngo@wcupa.edu>
2025-01-15 19:59:34 -0500

    Update entrypoint.sh

--- docker/master/entrypoint.sh
+++ docker/master/entrypoint.sh
@@ -6,6 +6,12 @@ sudo /usr/sbin/sshd -e
 cp -R /opt/home/student /home/student  
 cd /home/student/
 
+chmod 700 /home/student/.ssh
+chmod 600 /home/student/.ssh/id_rsa
+chmod 600 /home/student/.ssh/id_rsa.pub
+chmod 600 /home/student/.ssh/authorized_keys
+chmod 600 /home/student/.ssh/config
+
 jupyter lab --port=8888 --NotebookApp.token='' --no-browser --ip=0.0.0.0 &
 code-server --bind-addr 0.0.0.0:8088
 
